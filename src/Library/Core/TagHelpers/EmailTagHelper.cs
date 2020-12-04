29b88cb5c10cdfe738f4e17f42001768bd9fb9a2
Emrah Atalay <emrahatalay92@gmail.com>
2020-12-04 18:44:00 +0300

    email taghelper and Validation tool eklendi.

---
+++ src/Library/Core/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Core.TagHelpers
+{
+    [HtmlTargetElement("email", TagStructure = TagStructure.WithoutEndTag)]
+    public class EmailTagHelper : TagHelper
+    {
+        public string MailTo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + MailTo);
+            output.Content.SetContent(MailTo);
+        }
+    }
+}
---
+++ src/Library/Core/Validation/ValidationTool.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+
+namespace Core.Validation
+{
+    public static class ValidationTool
+    {
+        public static void Validate(Type validatorType, object entity)
+        {
+            var validator = (IValidator)Activator.CreateInstance(validatorType);
+
+            if (validator == null) return;
+
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
