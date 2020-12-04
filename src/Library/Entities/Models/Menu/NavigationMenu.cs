fbaf8db32fc16ee99be96b2a5ccc0c320dc4738a
Emrah Atalay <emrahatalay92@gmail.com>
2020-12-04 18:50:10 +0300

    Dinamik menü tablo classları oluşturuldu

---
+++ src/Library/Entities/Models/Menu/NavigationMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models.Menu
+{
+    [Table("AspNetNavigationMenu")]
+    public class NavigationMenu : BaseModel
+    {
+        public string Name { get; set; }
+
+        [ForeignKey("ParentNavigationMenu")]
+        public Guid? ParentMenuId { get; set; }
+
+        public string Area { get; set; }
+
+        public string ControllerName { get; set; }
+
+        public string ActionName { get; set; }
+
+        public bool IsExternal { get; set; }
+
+        public string ExternalUrl { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public bool Visible { get; set; }
+
+
+        public virtual NavigationMenu ParentNavigationMenu { get; set; }
+
+
+        [NotMapped]
+        public bool Permitted { get; set; }
+    }
+}
---
+++ src/Library/Entities/Models/Menu/RoleMenuPermission.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models.Menu
+{
+    [Table("AspNetRoleMenuPermission")]
+    public class RoleMenuPermission
+    {
+        public Guid RoleId { get; set; }
+
+        public Guid NavigationMenuId { get; set; }
+
+        public NavigationMenu NavigationMenu { get; set; }
+    }
+}
