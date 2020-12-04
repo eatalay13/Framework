b2f02608a97377b2a86cad7b0bf3a880de5a8476
Emrah Atalay <emrahatalay92@gmail.com>
2020-12-04 15:40:41 +0300

    Add project files.

---
+++ src/Library/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Contexts
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext()
+        {
+
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Test> Tests { get; set; }
+    }
+}
