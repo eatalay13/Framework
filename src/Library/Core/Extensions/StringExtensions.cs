9530583f3fdaef35d52a45acda81662d4b8c3f81
Emrah Atalay <emrahatalay92@gmail.com>
2020-12-04 15:56:21 +0300

    Added PagedList

---
+++ src/Library/Core/Extensions/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmptyWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+
+        public static Guid ToGuid(this string str)
+        {
+            return new Guid(str);
+        }
+
+        public static int ToInt(this string str)
+        {
+            return int.Parse(str);
+        }
+    }
+}
---
+++ src/Library/Core/Infrastructure/PagedList/IPagedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Infrastructure.PagedList
+{
+    public interface IPagedList<T> : IEnumerable<T>
+    {
+        int PageIndex { get; }
+
+        int PageSize { get; }
+
+        int TotalCount { get; }
+
+        int TotalPages { get; }
+
+        bool HasPreviousPage { get; }
+
+        bool HasNextPage { get; }
+    }
+}
