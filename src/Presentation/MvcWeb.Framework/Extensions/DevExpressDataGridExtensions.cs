beeb9ceb9310e1fe0ed952e49d63a12aad8299d7
Emrah Atalay <emrahatalay92@gmail.com>
2020-12-05 23:27:55 +0300

    Loglar DevExpress tablosu eklendi.

--- src/Presentation/MvcWeb.Framework/Extensions/DevExpressDataGridExtensions.cs
+++ src/Presentation/MvcWeb.Framework/Extensions/DevExpressDataGridExtensions.cs
@@ -17,6 +17,7 @@ namespace MvcWeb.Framework.Extensions
                             .FilterPanel(filter => filter.Visible(true))
                             .HeaderFilter(filter => filter.Visible(true))
                             .AllowColumnResizing(true)
+                            .ColumnResizingMode(ColumnResizingMode.NextColumn)
                             .ColumnAutoWidth(true)
                             .AllowColumnReordering(true)
                             .FilterRow(filterRow => filterRow
@@ -24,10 +25,16 @@ namespace MvcWeb.Framework.Extensions
                                 .ApplyFilter(GridApplyFilterMode.Auto))
                             .Sorting(sorting => sorting.Mode(GridSortingMode.Multiple))
                             .RowAlternationEnabled(true)
-                            .RemoteOperations(true)
                             .Export(export => export.Enabled(true).AllowExportSelectedData(true))
                             .ColumnChooser(c => c.Enabled(true).Mode(GridColumnChooserMode.DragAndDrop))
-                            .Selection(select => select.Mode(SelectionMode.Multiple));
+                            .Selection(select => select.Mode(SelectionMode.Multiple))
+                            .Paging(paging => paging.PageSize(10))
+                            .Pager(pager =>
+                            {
+                                pager.ShowPageSizeSelector(true);
+                                pager.AllowedPageSizes(new[] { 5, 10, 20, 50, 100 });
+                                pager.ShowInfo(true);
+                            });
 
             return dataGridBuilder;
         }
---
+++ src/Presentation/MvcWeb/Areas/Admin/DevExpApis/LogsApiController.cs
@@ -0,0 +1,49 @@
+﻿using Core.Helpers;
+using Data.Contexts;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcWeb.Controllers
+{
+    [Area(AreaDefaults.AdminAreaName)]
+    [Authorize(policy: PolicyDefaults.AuthorizationPolicy)]
+    public class LogsApiController : Controller
+    {
+        private AppDbContext _context;
+
+        public LogsApiController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(DataSourceLoadOptions loadOptions)
+        {
+            var log = _context.Log.Select(i => new
+            {
+                i.Message,
+                i.Level,
+                i.Timestamp,
+                i.Exception,
+                i.LogEvent,
+                i.Id
+            });
+
+            return Json(await DataSourceLoader.LoadAsync(log, loadOptions));
+        }
+
+        [HttpDelete]
+        public async Task Delete(int key)
+        {
+            var model = await _context.Log.FirstOrDefaultAsync(item => item.Id == key);
+
+            _context.Log.Remove(model);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
