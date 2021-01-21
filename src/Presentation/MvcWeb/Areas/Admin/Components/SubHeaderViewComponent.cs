673400b751a9fe17d72b9e6c3bc205a3f6757a7d
eatalayReos <eatalay@re-os.com>
2021-01-21 23:42:55 +0300

    Yeni tema giydirme bitti

---
+++ src/Presentation/MvcWeb/Areas/Admin/Components/SubHeaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWeb.Areas.Admin.Components
+{
+    public class SubHeaderViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
